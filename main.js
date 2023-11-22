773af5479dfa069a445d594ab67f07c607b7dcc1
deyvisongdias <deyvison.gregorio@estudante.ufjf.br>
2023-11-21 22:23:24 -0300

    Refatora criação dos Li

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const btnAdicioanar = document.querySelector("#adicioanar");
 const btnlimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
+adicionaElemetoNaLista();
+adicionaElemetoNaLista();
+adicionaElemetoNaLista();
 
-const li = document.createElement("li");
-li.textContent = "Primeiro";
-olItens.append(li);
 
-li2.textContent = "Segundo"
-olItens.appendChild=(li2);
-
-li3.textContent = "Segundo"
-olItens.appendChild=(li3);
-
+function dicionaElemetoNaLista(texto){
+    const li = document.createElement("li");
+    li.textContent = "Primeiro";
+    olItens.appendChild(li);
+}
